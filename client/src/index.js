8be9525b36f7042d45bf4ce4327b3ec77e47b39c
Vincent Le <vincentle.fh@gmail.com>
2022-03-17 22:38:37 +0200

    Finish PWA

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 
 ReactDOM.render(
     <React.StrictMode>
@@ -10,4 +10,4 @@ ReactDOM.render(
     document.getElementById("root")
 )
 
-serviceWorker.register()
+serviceWorkerRegistration.register()
