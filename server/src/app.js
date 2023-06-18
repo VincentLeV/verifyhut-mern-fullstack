98ea99cd89b6effa03a96dbf3ed9a02916a1211a
Vincent Le <vincentle.fh@gmail.com>
2023-06-18 11:40:44 +0300

    Fixes

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const config = require("./utils/config")
 const express = require("express")
 require("express-async-errors")
 const app = express()
-const cors = require( "cors" )
+const cors = require("cors")
 const {
     requestLogger,
     errorHandler,
